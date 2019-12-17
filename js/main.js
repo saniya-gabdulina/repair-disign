489d7c3cd8d7060caec394f72fbf15d296f577f6
saniya.gabdulina <mary_sorano@mail.ru>
2019-12-17 22:11:50 +0600

    feat: add jquery

--- js/main.js
+++ js/main.js
@@ -19,7 +19,11 @@ $(document).ready(function () {
     var modal = $('.modal'),
         modalBtn = $('[data-toggle=modal]'),
         closeBtn = $('.modal__close');
-    console.log(modal);
-    console.log(modalBtn);
-    console.log(closeBtn);
+  
+    modalBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+    });
+    closeBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+    });   
 });
